f6c210a497e247b95190f0a979cde2cfb1748d97
yangx <yangxushou@outlook.com>
2020-04-22 04:19:23 +0800

    readme.md

--- core/src/block.rs
+++ core/src/block.rs
@@ -1,12 +1,40 @@
+use chrono::prelude::*;
+use utils::coder;
+
 pub struct BlockHeader {
     pub time: i64,
-    pub tx_hash: String,
-    pub pre_has: String,
+    pub tx_hash: String, //交易数据的hash
+    pub pre_hash: String,
 }
 
 
 pub struct Block {
     pub header: BlockHeader,
     pub hash: String,
-    pub data: String,
+    pub data: String, //交易数据
+}
+
+impl Block {
+    fn set_hash(&mut self){
+
+        let header = coder::my_serialize(&(self.header));
+        self.hash = coder::get_hash(&header[..]);
+    }
+
+    pub fn new_block(data: String, pre_hash: String) -> Block{
+        let transactions = coder::my_serialize(&data);
+        let tx_hash = coder::get_hash(&transactions[..]);
+
+        let time = Utc::now().timestamp();
+        let mut block = Block{
+            header: BlockHeader{
+                time,
+                tx_hash, //交易数据的hash
+                pre_hash,
+            },
+            hash: "".to_string(),
+            data,
+        };
+
+    }
 }
--- utils/src/coder.rs
+++ utils/src/coder.rs
@@ -1,5 +1,7 @@
 use bincode;
 use serde::{Deserialize, Serialize};
+use crypto::digest::Digest;
+use crypto::sha3::Sha3;
 
 // pub fn serialize<T: ?Sized>(value: &T) -> Result<Vec<u8>> 
 // where
@@ -10,7 +12,7 @@ pub fn my_serialize<T: ?Sized>(value: &T) -> Vec<u8>
     where T: Serialize,
 {
     let serialized = bincode::serialize(value).unwrap();
-    serialize
+    serialized
 }
 
 
@@ -18,9 +20,39 @@ pub fn my_serialize<T: ?Sized>(value: &T) -> Vec<u8>
 // where
 //      T: Deserialize<'a>,
 
-pub fn my_deserialize<a', T>(bytes: &'a[u8]) -> T 
+pub fn my_deserialize<'a, T>(bytes: &'a[u8]) -> T
     where T: Deserialize<'a>,
 {
     let deserialized = bincode::deserialize(bytes).unwrap();
     deserialized
+}
+
+//hash函数
+pub fn get_hash(value: &[u8]) -> String{
+    let mut hasher = Sha3::sha3_256();
+    hasher.input(value);
+    hasher.result_str()
+
+}
+
+
+//测试序列化和反序列化
+#[derive(Serialize, Deserialize, PartialEq, Eq,Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+#[cfg(test)]
+mod tests {
+    use crate::coder::Point;
+    use crate::coder::{my_deserialize, my_serialize};
+
+    #[test]
+    fn it_works() {
+        let point = Point{x:1, y:1};
+        let se = my_serialize(&point);
+        let de: Point  = my_deserialize(&se);
+
+        assert_eq!(2 + 2, 4);
+    }
 }
